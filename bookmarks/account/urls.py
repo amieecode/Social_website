64d7e5fbf9a1bcf03a561ccc54141dbd993d7579
amieecode <enwelumchiamaka@gmail.com>
2024-08-01 23:56:05 +0100

    Added a register form to the system

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -1,19 +1,21 @@
 from django.contrib.auth import views as auth_views
-from django.urls import path 
+from django.urls import include, path 
 from . import views 
 
 urlpatterns = [
     #path('login/', views.user_login, name='login'),
     #login/ logged out url
-    path('login/', auth_views.LoginView.as_view(), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    #path('login/', auth_views.LoginView.as_view(), name='login'),
+    #path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     #change password url
-    path('password-change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
-    path('password-change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
+    #path('password-change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
+    #path('password-change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
     #rest password urls
-    path('password_rest/', auth_views.PasswordResetView.as_view(), name='password_reset'),
-    path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done' ),
-    path('password-reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
-    path('password-reset/complete/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+    #path('password_rest/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    #path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done' ),
+    #path('password-reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    #path('password-reset/complete/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+    path('', include('django.contrib.auth.urls')),
     path('', views.dashboard, name='dashboard'),
+    path('register/', views.register, name='register'),
 ]
