4cfd628c1140b14926cd486a16e1d8d649247774
amieecode <enwelumchiamaka@gmail.com>
2024-08-04 23:57:55 +0100

    Added user profile for social authenticators

--- bookmarks/bookmarks/settings.py
+++ bookmarks/bookmarks/settings.py
@@ -79,6 +79,19 @@ AUTHENTICATION_BACKENDS =[
     'social_core.backends.google.GoogleOAuth2',
 ]
 
+SOCIAL_AUTH_PIPELINE = [
+    'social_core.pipeline.social_auth.social_details',
+    'social_core.pipeline.social_auth.social_uid',
+    'social_core.pipeline.social_auth.auth_allowed',
+    'social_core.pipeline.social_auth.social_user',
+    'social_core.pipeline.user.get_username',
+    'social_core.pipeline.user.create_user',
+    'account.authentication.create_profile',
+    'social_core.pipeline.social_auth.associate_user',
+    'social_core.pipeline.social_auth.load_extra_data',
+    'social_core.pipeline.user.user_details',
+]
+
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
