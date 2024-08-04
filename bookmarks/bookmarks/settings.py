988a2cc050c838183bb481b9523d811fe79416b2
amieecode <enwelumchiamaka@gmail.com>
2024-08-04 12:44:16 +0100

    Added email and username login authentication

--- bookmarks/bookmarks/settings.py
+++ bookmarks/bookmarks/settings.py
@@ -70,6 +70,10 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'bookmarks.wsgi.application'
 
+AUTHENTICATION_BACKENDS =[
+    'django.contrib.auth.backends.ModelBackend',
+    'account.authentication.EmailAuthBackend',
+]
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
