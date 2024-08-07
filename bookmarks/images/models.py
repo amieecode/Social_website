a9786a4d7b43bf79b41c2838ce65ed63027446cf
amieecode <enwelumchiamaka@gmail.com>
2024-08-08 00:39:48 +0100

    Added like/unlike button to bookmarked images

--- bookmarks/images/models.py
+++ bookmarks/images/models.py
@@ -12,7 +12,7 @@ class Image(models.Model):
     image = models.ImageField(upload_to='images/%Y/%m/%d/')
     description = models.TextField(blank=True)
     created = models.DateTimeField(auto_now_add=True)
-    user_like = models.ManyToManyField( settings.AUTH_USER_MODEL, related_name='image_liked', blank=True )
+    users_like = models.ManyToManyField( settings.AUTH_USER_MODEL, related_name='image_liked', blank=True )
 
     class Meta:
         indexes = [
